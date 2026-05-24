60c3db17044cb86df5ee35d3a87bf7301b29e925
arbaselu <marioarbaselu5@gmail.com>
2026-05-24 19:50:35 +0300

    Implementare logica citire senzor, functionare pompa de apa

--- src/main.c
+++ src/main.c
@@ -17,7 +17,40 @@ void pump_off(void)
     pump_state = 0;
 }
 
-int main(void) {
-    
-   
+int main(void)
+{
+    Timer0_Init();
+    ADC_Init();
+    USART_Init_Default();
+
+    GPIO_Init(GPIO_PORTD, 7, GPIO_OUTPUT);
+
+    uint32_t last_time = 0;
+    uint16_t adc_value = 0;
+    char text[60];
+
+    pump_off();
+
+    while (1)
+    {
+        if (Millis() - last_time >= 500)
+        {
+            last_time = Millis();
+
+            adc_value = ADC_Read(0);
+
+            if (adc_value > 580)
+                pump_on();
+
+            if (adc_value < 550)
+                pump_off();
+
+    if (pump_state == 0)
+    {
+    sprintf(text, "ADC=%u Pump=%u\r\n", adc_value, pump_state);
+    USART_Transmit(text, strlen(text));
     }
+
+    }
+    }
+}
